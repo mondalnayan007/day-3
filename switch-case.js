fa9752bcf2d6ff27963ca539190b0b10d5d71030
Nayan <n007kingkong@gmail.com>
2025-07-23 00:19:03 +0600

    grading using switch case

---
+++ switch-case.js
@@ -0,0 +1,25 @@
+
+
+var  result = "d";
+
+
+switch(result){
+    case "A":
+        console.log("very good");
+        break;
+    case "B":
+        console.log(" good");
+        break;
+    case "C":
+        console.log("bad");
+        break;
+    case "D":
+        console.log("worst");
+        break;
+
+        default:
+            console.log("Invalid")
+}
+
+
+
