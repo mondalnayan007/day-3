f77181707536a0a14b3a045c38cf615573612416
Nayan <n007kingkong@gmail.com>
2025-07-22 22:09:54 +0600

    for loop

---
+++ for.js
@@ -0,0 +1,12 @@
+
+
+var friends = ['sakib' , 'rakib', 'mofiz' , 'kuddus' , 'mokhles' , 'salam'];
+var i ;
+
+for(i= 0; i<friends.length; i++)
+{
+    var names = friends[i];
+    console.log(names);
+}
+var name = friends[3];
+ console.log(name);
---
+++ name.js
@@ -0,0 +1,14 @@
+
+
+var letters = [ 'A' ,'B' ,'C' , 'D' , 'E'];
+var i = Math.floor(Math.random()*5)
+var name = '';
+
+// for( i; i<letters.length;i++)
+// {
+//     var name = console.log(letters[i])
+    
+// }
+
+
+
